a6547a13b343632a3d43c2cb07c797271e2ac834
supersu <soo4826@konkuk.ac.kr>
2024-07-30 17:04:58 +0900

    Update support for V3 Dataset

--- tools/preprocess_rscube_dataset.py
+++ tools/preprocess_rscube_dataset.py
@@ -17,14 +17,14 @@ def print_config(root_path, save_path, scenarios, sample_rate):
 def load_label_files(root_path, scenarios):
     label_file_list = []
     for scenario in scenarios:
-        label_file_list += glob.glob(str(root_path / scenario / 'labels' / '**/*.txt'), recursive=True)
+        label_file_list += glob.glob(str(root_path / scenario / '**/*.txt'), recursive=True)
     label_file_list.sort()
     return label_file_list
 
 def copy_label_files(label_file_list, root_path, save_path, scenarios):
     for label_file in track(label_file_list, description="Copying label files..."):
         scenario = next(s for s in scenarios if f'/{s}/' in label_file)
-        save_label_file = str(label_file).replace(str(root_path), str(save_path)).replace(f'/{scenario}/labels', '/labels')
+        save_label_file = str(label_file).replace(str(root_path), str(save_path)).replace(f'/{scenario}', '/labels')
         save_label_file = Path(save_label_file)
         save_label_file.parent.mkdir(parents=True, exist_ok=True)
         shutil.copy(label_file, save_label_file)
@@ -37,7 +37,7 @@ def convert_pcd_to_npy(label_file, root_path, save_path, scenarios):
     points = np.concatenate([xyz, intensity], axis=1)
     
     scenario = next(s for s in scenarios if f'/{s}/' in label_file)
-    save_npy_file = str(label_file).replace(str(root_path), str(save_path)).replace(f'/{scenario}/labels', '/points').replace('.txt', '.npy')
+    save_npy_file = str(label_file).replace(str(root_path), str(save_path)).replace(f'/{scenario}', '/points').replace('.txt', '.npy')
     save_npy_file = Path(save_npy_file)
     save_npy_file.parent.mkdir(parents=True, exist_ok=True)
     np.save(save_npy_file, points) 
@@ -46,25 +46,38 @@ def convert_and_save_pcd(label_file_list, root_path, save_path, scenarios):
     with Pool(cpu_count()) as pool:
         pool.starmap(convert_pcd_to_npy, [(label_file, root_path, save_path, scenarios) for label_file in label_file_list])
 
-def create_image_sets(label_file_list, save_path, sample_rate):
+def create_image_sets(label_file_list, save_path, sample_rate, split='test'):
     sampled_label_file_list = label_file_list[::sample_rate]
     ImageSets = save_path / 'ImageSets'
     ImageSets.mkdir(parents=True, exist_ok=True)
-    with open(ImageSets / 'test.txt', 'w') as f:
+    with open(ImageSets / (split+'.txt'), 'w') as f:
         for label_file in sampled_label_file_list:
             f.write(label_file.split('/')[-1].replace('.txt', '') + '\n')
-    print(f"Saved into {ImageSets / 'test.txt'}")
+    print(f"Saved into {ImageSets / (split+'.txt')}")
 
 def main():
-    root_path = Path("/home/ailab/AILabDataset/02_Custom_Dataset/21_RSCube_Dataset/V2/Raw")
-    save_path = Path("/home/ailab/AILabDataset/02_Custom_Dataset/21_RSCube_Dataset/V2/Processed")
-    scenarios = ['highway', 'urban']
-    sample_rate = 2
+    # V2 Configuration
+    # root_path = Path("/home/ailab/AILabDataset/02_Custom_Dataset/21_RSCube_Dataset/V2/Raw")
+    # save_path = Path("/home/ailab/AILabDataset/02_Custom_Dataset/21_RSCube_Dataset/V4/Processed")
+    # scenarios = ['test']
+    # split = {'test': ['urban', 'highway']}
+    # sample_rate = 2
+
+    # V3 Configuration
+    root_path = Path("/home/ailab/AILabDataset/02_Custom_Dataset/21_RSCube_Dataset/V3/Raw")
+    save_path = Path("/home/ailab/AILabDataset/02_Custom_Dataset/21_RSCube_Dataset/V3/Processed")
+    scenarios = ['train', 'val']
+    split = {'train': ['1_1', '2_6', '2_8', '2_9', '2_10', '2_11', '2_12', '2_13'],
+             'val': ['1_2', '2_7', '2_14']}
+    sample_rate = 1
 
     print_config(root_path, save_path, scenarios, sample_rate)
-
-    label_file_list = load_label_files(root_path, scenarios)
-    # copy_label_files(label_file_list, root_path, save_path, scenarios)
+    for scenario in scenarios:
+        label_file_list = load_label_files(root_path, split[scenario])
+        label_file_list.sort()
+        create_image_sets(label_file_list, save_path, sample_rate, split=scenario)
+        copy_label_files(label_file_list, root_path, save_path, split[scenario])
+        convert_and_save_pcd(label_file_list, root_path, save_path, split[scenario])
     
     # Multiprocessing
     # convert_and_save_pcd(label_file_list, root_path, save_path, scenarios)
@@ -73,7 +86,5 @@ def main():
     # for label_file in label_file_list:
     #     convert_pcd_to_npy(label_file, root_path, save_path, scenarios)
     
-    create_image_sets(label_file_list, save_path, sample_rate)
-
 if __name__ == "__main__":
     main()
